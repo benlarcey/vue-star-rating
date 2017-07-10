e5afdbaa3ea03c90eb53317cdfc2bdbbb984cf75
Ben Larcey <ben@benlarcey.com>
2017-07-10 18:16:12 +0100

    Add box-shadow

--- src/star.vue
+++ src/star.vue
@@ -6,8 +6,14 @@
             <stop :offset="getFill" :stop-color="(rtl) ? activeColor : inactiveColor" />
         </linearGradient>
 
-        <polygon :points="starPointsToString" :fill="getGradId" :stroke="borderColor" :stroke-width="borderWidth" />
+        <polygon :points="starPointsToString" :fill="getGradId" :stroke="borderColor" :stroke-width="borderWidth" :filter=" fill > 0 ? 'url(#filter-3)' : '' " stroke-linejoin="round" />
+        <filter x="-50%" y="-50%" width="200%" height="200%" filterUnits="objectBoundingBox" id="filter-3">
+            <feOffset dx="0" dy="2" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset>
+            <feGaussianBlur stdDeviation="1.5" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur>
+            <feColorMatrix values="0 0 0 0 0.989530187   0 0 0 0 0.514345435   0 0 0 0 0.0230749819  0 0 0 0.5 0" type="matrix" in="shadowBlurOuter1"></feColorMatrix>
+        </filter>
         <polygon :points="starPointsToString" :fill="getGradId" />
+
     </svg>
 </template>
 
